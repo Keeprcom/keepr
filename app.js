7b0d4156669cfe69a85260e444649b4e018ab577
apedneka <pednekar.ameya@gmail.com>
2016-04-19 12:28:35 -0400

    clean up

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require ('express');
 var app = express();
-var logger = require('./logger'); // ./ signifies local module and not NPM module
+var logger = require('./logger');
 app.use(logger);
 
 var Twitter = require('twitter');
@@ -18,20 +18,20 @@ app.get('/', function(req,res){
 app.get('/home/:query',function(req,res){
 	client.get('search/tweets', {q: req.params.query}, function(err, data, response) {
 	   if (err) throw err;
-	  // else res.json(data);
+	  
 	  else {
-	  var ids = {numbers:[]};
-	  var text = "";
-	  for(var i in data.statuses){
-	  	var item = data.statuses[i];
-    	ids.numbers.push({
-    		"status_id":item.user.id
-    	});
-    	text+=item.text;
-    	var cleanString = text.replace(/[#.|&;:$%@"<>\/-=?()+,]/g, " ");   
-		}
-	}
-		res.json(cleanString);
+	  		var ids = {numbers:[]};
+	  		var text = "";
+			  for(var i in data.statuses){
+			  		var item = data.statuses[i];
+			    		ids.numbers.push({
+			    		"status_id":item.user.id
+			    		});
+		    		text+=item.text;
+		    		var cleanString = text.replace(/[#.|&;:$%@"<>\/-=?()+,]/g, " ");   
+				}
+			}
+	res.json(cleanString);
 	});	
 });
 
