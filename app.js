ad60acce84f8defc90ad083c81066fca3c31190b
apedneka <pednekar.ameya@gmail.com>
2016-04-19 12:01:35 -0400

    search function working, ids stripped in to array, and text string cleaned

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require ('express');
+var app = express();
+var logger = require('./logger'); // ./ signifies local module and not NPM module
+app.use(logger);
+
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'm92WpTmzACSHAS0uK5qA',
+  consumer_secret: 'M7twTg4YoC7NDpDJXzczFp2S4z3Ek2R33LUy9rKvMU',
+  access_token_key: '1386070296-zItk7uzA7sABC1R83kBAY5QKbWMUTdge6oCBgki',
+  access_token_secret: 'qsuaqgMKDE9pdyuY2186NoIfgOZPN95mg8UXZ77DgaI'
+});
+
+app.set('view engine', 'ejs');
+
+// resonse.json(object here) serializes json to client
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/home/:query',function(req,res){
+	client.get('search/tweets', {q: req.params.query}, function(err, data, response) {
+	   if (err) throw err;
+	  // else res.json(data);
+	  else {
+	  var ids = {numbers:[]};
+	  var text = "";
+	  for(var i in data.statuses){
+	  	var item = data.statuses[i];
+    	ids.numbers.push({
+    		"status_id":item.user.id
+    	});
+    	text+=item.text;
+    	var cleanString = text.replace(/[#.|&;:$%@"<>\/-=?()+,]/g, " ");   
+		}
+	}
+		res.json(cleanString);
+	});	
+});
+
+app.get('/test',function(req,res){
+	res.render('index')
+});
+
+
+app.listen(3000, function (){
+console.log('listening on port 3000');
+});
