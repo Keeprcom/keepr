bb59fea27c6329cc5f1df0bd81e041731d0d4895
apedneka <pednekar.ameya@gmail.com>
2016-05-11 20:58:29 -0400

    created /latest route, extracting text + urls and sending as json response from restful api

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require ('express');
 var app = express();
 var logger = require('./logger');
-app.use(logger);
-
-app.set('port', (process.env.PORT || 3000));
-
 var routes=require('./routes/twitter');
 
-app.get('/:query', routes.query);
+app.use(logger);
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/ask/:query', routes.query);
+app.get('/latest', routes.latest);
 
 app.listen(app.get('port'), function (){
 console.log('listening on port ' + app.get('port'));
