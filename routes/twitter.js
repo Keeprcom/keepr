5475e2c9e989f7d864f9f5546c62d47a6e07c52b
apedneka <pednekar.ameya@gmail.com>
2016-07-12 18:43:48 -0400

    uncommented

--- routes/twitter.js
+++ routes/twitter.js
@@ -28,30 +28,30 @@ module.exports ={
 			    		});
 		    		// text+=item.text;	   
 					}
-				// var cleanString = text.replace(/[|$<>\\-\\()+,]/g, " ");
-				// var words = []; 
-				// var splitted = cleanString.split(" ");
-				// var regexForMentions=/(^|[^@\w])@(\w{1,15})\b/g;
-				// var urlExpression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-				// var mentions=String(splitted).toLowerCase().match(regexForMentions,String(cleanString));
-				// var urls=(String)(splitted).toLowerCase().match(urlExpression);
-				// var unique_mentions=functions.uniq(mentions);
-				// var unique_urls=functions.uniq(urls);
+				var cleanString = text.replace(/[|$<>\\-\\()+,]/g, " ");
+				var words = []; 
+				var splitted = cleanString.split(" ");
+				var regexForMentions=/(^|[^@\w])@(\w{1,15})\b/g;
+				var urlExpression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+				var mentions=String(splitted).toLowerCase().match(regexForMentions,String(cleanString));
+				var urls=(String)(splitted).toLowerCase().match(urlExpression);
+				var unique_mentions=functions.uniq(mentions);
+				var unique_urls=functions.uniq(urls);
 				
-				// for(var i=0; i<splitted.length; i++) {
-			 //    	words[splitted[i]] = ( typeof words[splitted[i]] != 'undefined' ) ? words[splitted[i]]+=1 : 1;
-				// }
+				for(var i=0; i<splitted.length; i++) {
+			    	words[splitted[i]] = ( typeof words[splitted[i]] != 'undefined' ) ? words[splitted[i]]+=1 : 1;
+				}
 
-				// var words_counter = 0
+				var words_counter = 0
 					
-				// for (key in words) {
-			 //  		words_counter++;
-			 //  		console.log(key+" : "+words[key]);
-				// }
-				// console.log(unique_mentions);	
-				// console.log(unique_urls);			
-				// console.log("number of unique words: " +words_counter+ " total number of words: "+splitted.length) ;
-				// console.log("-----------------------------");
+				for (key in words) {
+			  		words_counter++;
+			  		console.log(key+" : "+words[key]);
+				}
+				console.log(unique_mentions);	
+				console.log(unique_urls);			
+				console.log("number of unique words: " +words_counter+ " total number of words: "+splitted.length) ;
+				console.log("-----------------------------");
 				}
 		res.json(ids);
 		});	
